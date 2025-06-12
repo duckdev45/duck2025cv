1c7a10c6617420016b885ae1f5d20e5e9ff037fc
duck <yth@hismax.onmicrosoft.com>
2025-06-13 00:18:06 +0800

    feat: update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import duckImage from './assets/images/duck.png'
+
 function App() {
   return (
-    <>
-      {/*    tbd */}
-    </>
+    <div className="bg-[#5cc278] min-h-screen flex justify-center items-center">
+      <div className={'flex flex-col'}>
+        <div className={'rounded-full h-60 w-60 overflow-hidden'}>
+          <img src={duckImage} alt="duck" className={'w-full h-full object-cover'} />
+        </div>
+        <div>
+          <p className="font-pixel mt-2 text-4xl font-bold text-white text-right">
+            YATZU HSU </p>
+          <p className="font-pixel mt-4 text-3xl font-bold text-white">
+            FrontEnd Developer</p>
+        </div>
+        <h1 className="font-pixel mt-36 text-4xl font-bold text-center text-white animate-bounce ">
+          ▼ </h1>
+      </div>
+    </div>
   )
 }
 
